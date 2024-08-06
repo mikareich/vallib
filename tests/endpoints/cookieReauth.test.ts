04a6789c86ef116cf96e1e4489290d9adffd06ae
Mika Reich <mikareich0@gmail.com>
2024-08-06 15:15:23 +0200

    comment cookieReauth test file because of test runner

--- tests/endpoints/cookieReauth.test.ts
+++ tests/endpoints/cookieReauth.test.ts
@@ -1,18 +1,16 @@
-import { COOKIE_REAUTH } from "~/src/endpoints/auth";
-
 describe("Cookie Reauth: Get a new token using the cookies from a previous authorization request", () => {
   it("should return a new token", async () => {
-    const cookiesWithSSID = [`ssid=${process.env.SSID_COOKIE}`];
+    const _cookiesWithSSID = [`ssid=${process.env.SSID_COOKIE}`];
 
     console.warn(
       "Skipping test because it will invalidate the current token used for testing",
       "You have to explicitly enable this test to run by removing this line",
     );
 
-    const response = await COOKIE_REAUTH({
-      cookies: cookiesWithSSID,
-      proxy: process.env.PROXY_URL,
-    });
+    // const response = await COOKIE_REAUTH({
+    //   cookies: cookiesWithSSID,
+    //   proxy: process.env.PROXY_URL,
+    // });
 
     // expect(response.status).toBe(200);
   });
