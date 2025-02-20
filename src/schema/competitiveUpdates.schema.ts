d6824916ff30672dcf9a13683d3ceae68c32a412
Mika Reich <mikareich0@gmail.com>
2025-02-20 22:38:26 +0100

    bump version to 0.0.37 and refactor competitive updates schema for improved structure

--- src/schema/competitiveUpdates.schema.ts
+++ src/schema/competitiveUpdates.schema.ts
@@ -1,35 +1,28 @@
-// automatically generated schema, 2025-02-19T23:57:35.263Z
+import { z } from "zod";
 
-import { z } from "zod"
-
-const competitiveUpdates_SCHEMA = 
-  z.union([
-    z.string(),
+const competitiveUpdates_SCHEMA = z.object({
+  Version: z.number(),
+  Subject: z.string(),
+  Matches: z.array(
     z.object({
-      Version: z.number(),
-      Subject: z.string(),
-      Matches: z.array(
-        z.object({
-          MatchID: z.string(),
-          MapID: z.string(),
-          SeasonID: z.string(),
-          MatchStartTime: z.number(),
-          TierAfterUpdate: z.number(),
-          TierBeforeUpdate: z.number(),
-          RankedRatingAfterUpdate: z.number(),
-          RankedRatingBeforeUpdate: z.number(),
-          RankedRatingEarned: z.number(),
-          RankedRatingPerformanceBonus: z.number(),
-          RankedRatingRefundApplied: z.number(),
-          CompetitiveMovement: z.string(),
-          AFKPenalty: z.number(),
-          WasDerankProtected: z.boolean(),
-          WasDerankProtectionReplenished: z.boolean(),
-          IsDerankProtectedTier: z.boolean(),
-        })
-      ),
+      MatchID: z.string(),
+      MapID: z.string(),
+      SeasonID: z.string(),
+      MatchStartTime: z.number(),
+      TierAfterUpdate: z.number(),
+      TierBeforeUpdate: z.number(),
+      RankedRatingAfterUpdate: z.number(),
+      RankedRatingBeforeUpdate: z.number(),
+      RankedRatingEarned: z.number(),
+      RankedRatingPerformanceBonus: z.number(),
+      RankedRatingRefundApplied: z.number(),
+      CompetitiveMovement: z.string(),
+      AFKPenalty: z.number(),
+      WasDerankProtected: z.boolean(),
+      WasDerankProtectionReplenished: z.boolean(),
+      IsDerankProtectedTier: z.boolean(),
     }),
-  ])
+  ),
+});
 
-
-export default competitiveUpdates_SCHEMA
+export default competitiveUpdates_SCHEMA;
